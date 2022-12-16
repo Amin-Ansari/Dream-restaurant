672bc4ad6d9850d0a8d0253505886d44e82ab7d0
Amin Ansari <devansari@yahoo.com>
2022-12-16 16:27:13 +0330

    The fisrt phase of animatin is added

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ import { closeCanvas } from "./function";
 import { menuButtons } from "./elements";
 import { selectTheButton } from "./function";
 import { scrollTheTitle } from "./function";
+// import { obs } from "./class";
 
 toggleButton.addEventListener("click", toggleSub);
 cartButton.addEventListener("click", toggleCart);
@@ -17,3 +18,21 @@ offCanvasCloseBtn.addEventListener("click", closeCanvas);
 menuButtons.forEach((item) => item.addEventListener("click", selectTheButton));
 
 scrollTheTitle();
+
+let obs = new IntersectionObserver(function (entry) {
+  for (let element of entry) {
+    if (element.isIntersecting) {
+      element.target.classList.replace("img-anim-up", "scale-up-img");
+      element.target.classList.replace("h-anim-right", "move-to-right-title");
+      element.target.classList.replace("p-anim-right", "move-to-right-text");
+      element.target.classList.replace(
+        "button-anim-right",
+        "move-to-right-btn"
+      );
+    }
+  }
+});
+
+let intersectedElements = document.querySelectorAll(".observer");
+
+intersectedElements.forEach((item) => obs.observe(item));
